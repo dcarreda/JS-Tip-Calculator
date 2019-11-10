e46c0c318838c3098cbc9bcaa9ca7d9ae21f989b
dcarreda <diogo.arreda@gmail.com>
2019-11-10 13:45:23 +0000

    Tip Calculator Functional

--- js/script.js
+++ js/script.js
@@ -3,11 +3,36 @@ const CALBUTTON = document.querySelector("#calculate");
 const PEOPLEAMT = document.querySelector("#peopleamt");
 const TIPPERC = document.querySelector("#serviceQuality");
 
-function showResult(){
-    console.log(BILLAMT.value);
-    console.log(TIPPERC.value);
-    console.log(PEOPLEAMT.value);
+const SHOWTOTAL = document.querySelector("#tip");
+const SHOWEACH = document.querySelector("#totaleach");
 
+document.querySelector("#totalTip").style.display = "none";
+function calculateTip(){
+
+    if(BILLAMT.value === "" || TIPPERC.value === 0){
+        alert("please enter valid values");
+        return;
+    }
+    if(PEOPLEAMT.value=== ""){
+        PEOPLEAMT.value = 1;
+    }
+    document.querySelector("#totalTip").style.display = "block";
+
+    var temp = 0.0;
+    //Calculating the tip
+    var tip = parseFloat(BILLAMT.value) * parseFloat(TIPPERC.value);
+    tip=tip.toFixed(2);
+    var total = parseFloat(tip) + parseInt(BILLAMT.value);
+
+    
+    document.querySelector("#tip").innerHTML=total;
+
+    //Total Each tip
+    var total_e = total / parseInt(PEOPLEAMT.value);
+    total_e=total_e.toFixed(2);
+    document.querySelector("#totaleach").innerHTML=total_e;
+
+    //console.log("total tip each is: " + total_e);
 }
 
-CALBUTTON.addEventListener('click', showResult); // if showResult(), then the function is called right now, and not in an event case
+CALBUTTON.addEventListener('click', calculateTip); // if showResult(), then the function is called right now, and not in an event case
